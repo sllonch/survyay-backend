aa637865573e6630063b9576e323af0f8ed95864
kovitx <sllonk@gmail.com>
2018-12-17 18:47:59 +0100

    fixes in API for delete survey

--- routes/API.js
+++ routes/API.js
@@ -1,107 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const bcrypt = require('bcrypt')
-
 const User = require('../models/user')
 const Survey = require('../models/survey')
-
 const { isLoggedIn } = require('../helpers/middlewares')
-const ObjectId = require('mongoose').Types.ObjectId
+// const ObjectId = require('mongoose').Types.ObjectId
 
-/*
-router.get('/me', (req, res, next) => {
-  if (req.session.currentUser) {
-    res.json(req.session.currentUser)
-  } else {
-    res.status(404).json({
-      error: 'not-found'
-    })
-  }
-})
-
-router.post('/login', (req, res, next) => {
-  if (req.session.currentUser) {
-    return res.status(401).json({
-      error: 'unauthorized'
-    })
-  }
-
-  const { email, password } = req.body
-
-  if (!email || !password) {
-    return res.status(422).json({
-      error: 'validation'
-    })
-  }
-
-  User.findOne({
-    email
-  })
-    .then((user) => {
-      if (!user) {
-        return res.status(404).json({
-          error: 'not-found'
-        })
-      }
-      // TODO async bcrypt
-      if (bcrypt.compareSync(password, user.password)) {
-        req.session.currentUser = user
-        return res.status(200).json(user)
-      }
-      return res.status(404).json({
-        error: 'not-found'
-      })
-    })
-    .catch(next)
-})
-
-router.post('/signup', (req, res, next) => {
-  const {
-    name,
-    email,
-    password
-  } = req.body
-
-  console.log(req.body)
-
-  if (!name || !email || !password) {
-    return res.status(422).json({
-      error: 'empty'
-    })
-  }
-
-  User.findOne({
-    email
-  }, 'email')
-    .then((userExists) => {
-      if (userExists) {
-        return res.status(422).json({
-          error: 'email-not-unique'
-        })
-      }
-
-      const salt = bcrypt.genSaltSync(10)
-      const hashPass = bcrypt.hashSync(password, salt)
-
-      const newUser = User({
-        name,
-        email,
-        password: hashPass
-      })
-
-      return newUser.save().then(() => {
-        req.session.currentUser = newUser
-        res.json(newUser)
-      })
-    })
-    .catch(next)
-})
-
-router.post('/logout', (req, res) => {
-  req.session.currentUser = null
-  return res.status(204).send()
-})
-*/
 router.get('/surveys', isLoggedIn(), (req, res, next) => {
   Survey.find()
     .then((surveys) => {
@@ -131,7 +34,7 @@ router.post('/survey/new', isLoggedIn(), (req, res, next) => {
     })
   }
 
-  const emails = participants.map(participant => participant.email) // Passing from an array of objects to an array of emails
+  const emails = participants.map(participant => participant.email) // Converting an array of objects with the key "email" to an array of emails
 
   User.find({ email: { $in: emails } })
     .then((users) => {
@@ -223,6 +126,18 @@ router.put('/survey/:id/vote', isLoggedIn(), (req, res, next) => {
         .catch(() => {
           res.json('Error').status(500)
         })
+    })
+    .catch(() => {
+      res.json('Error').status(500)
+    })
+})
+
+router.delete('/survey/:id/delete', isLoggedIn(), (req, res, next) => {
+  const id = req.params.id
+  console.log(id)
+  Survey.findByIdAndDelete(id)
+    .then((survey) => {
+      res.status(200).json(survey)
     })
     .catch(() => {
       res.json('Error').status(500)
