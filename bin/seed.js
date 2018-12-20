7ed138eb17b143bee00475d2da9dae3e5358a830
kovitx <sllonk@gmail.com>
2018-12-20 09:59:32 +0100

    changes in seed

--- bin/seed.js
+++ bin/seed.js
@@ -117,6 +117,11 @@ const users = [
     name: 'Sergi',
     email: 'llonkovitx@gmail.com',
     password: '123456'
+  },
+  {
+    name: 'User',
+    email: 'survyays@gmail.com',
+    password: '123456'
   }
 ]
 
@@ -130,11 +135,13 @@ users.forEach(user => {
 
 const surveys = [
   {
-    title: 'Is Alejo Daudí the funniest man on Earth?',
+    title: 'What is your favorite music artist of 2018?',
     answers: [
-      { answerTitle: 'Yes', votes: 5 },
-      { answerTitle: 'Of course', votes: 5 },
-      { answerTitle: 'Not really', votes: 2 }
+      { answerTitle: 'Cardi B', votes: 1 },
+      { answerTitle: 'Idles', votes: 3 },
+      { answerTitle: 'Low', votes: 3 },
+      { answerTitle: 'Rosalía', votes: 3 },
+      { answerTitle: 'Mitski', votes: 2 }
     ]
   },
   {
@@ -146,6 +153,14 @@ const surveys = [
       { answerTitle: '3', votes: 1 },
       { answerTitle: '4', votes: 2 },
       { answerTitle: '4+', votes: 5 }
+    ]
+  },
+  {
+    title: 'Is Alejo Daudí the funniest man on Earth?',
+    answers: [
+      { answerTitle: 'Yes', votes: 5 },
+      { answerTitle: 'Of course', votes: 5 },
+      { answerTitle: 'Not really', votes: 2 }
     ]
   }
 ]
@@ -173,9 +188,10 @@ User.create(users)
       { participant: users[16]._id, hasVoted: true },
       { participant: users[17]._id, hasVoted: false },
       { participant: users[18]._id, hasVoted: false },
-      { participant: users[19]._id, hasVoted: false }
+      { participant: users[19]._id, hasVoted: false },
+      { participant: users[20]._id, hasVoted: false }
     ]
-    surveys[0].owner = users[18]._id
+    surveys[0].owner = users[20]._id
     surveys[1].participants = [
       { participant: users[0]._id, hasVoted: true },
       { participant: users[1]._id, hasVoted: true },
@@ -196,9 +212,34 @@ User.create(users)
       { participant: users[16]._id, hasVoted: true },
       { participant: users[17]._id, hasVoted: false },
       { participant: users[18]._id, hasVoted: false },
-      { participant: users[19]._id, hasVoted: false }
+      { participant: users[19]._id, hasVoted: false },
+      { participant: users[20]._id, hasVoted: false }
     ]
-    surveys[1].owner = users[18]._id
+    surveys[1].owner = users[20]._id
+    surveys[2].participants = [
+      { participant: users[0]._id, hasVoted: true },
+      { participant: users[1]._id, hasVoted: true },
+      { participant: users[2]._id, hasVoted: false },
+      { participant: users[3]._id, hasVoted: true },
+      { participant: users[4]._id, hasVoted: true },
+      { participant: users[5]._id, hasVoted: false },
+      { participant: users[6]._id, hasVoted: true },
+      { participant: users[7]._id, hasVoted: true },
+      { participant: users[8]._id, hasVoted: false },
+      { participant: users[9]._id, hasVoted: true },
+      { participant: users[10]._id, hasVoted: true },
+      { participant: users[11]._id, hasVoted: false },
+      { participant: users[12]._id, hasVoted: true },
+      { participant: users[13]._id, hasVoted: true },
+      { participant: users[14]._id, hasVoted: false },
+      { participant: users[15]._id, hasVoted: true },
+      { participant: users[16]._id, hasVoted: true },
+      { participant: users[17]._id, hasVoted: false },
+      { participant: users[18]._id, hasVoted: false },
+      { participant: users[19]._id, hasVoted: false },
+      { participant: users[20]._id, hasVoted: false }
+    ]
+    surveys[2].owner = users[20]._id
     Survey.create(surveys)
       .then(() => {
         console.log('surveys created')
